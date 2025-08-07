976bc78179ab2f9432087711b3604f404be69a4d
skhemot <sagarkhemot123@gmail.com>
2025-08-07 15:39:59 +0530

    Update Car.java

--- Car.java
+++ Car.java
@@ -5,7 +5,7 @@ public class Car extends Vehicle {
 	private int noOfDoors;
 	private boolean isElectric;
 private int =10;
-private int a=0= ;
+private int a=0 ;
 //pim q a 
 	//pim q a 
 	//pim q a 
@@ -38,6 +38,7 @@ private int a=0= ;
 	}
 
 }
+
 
 
 
